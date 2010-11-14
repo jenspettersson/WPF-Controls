02797e860fd3219b97a8f16afc061aabcee8c0e6
Jens Pettersson <jens.pettersson@live.se>
2010-11-15 00:37:02 +0100

    Added Right, Left, RightReplace and LeftReplace transitions for the TransitioningContentControl

---
+++ src/TestApplication/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestApplication
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			this.InitializeComponent();
+
+			// Insert code required on object creation below this point.
+		}
+
+		private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: Add event handler implementation here.
+			
+			
+		}
+	}
+}
